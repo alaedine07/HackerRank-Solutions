577b87f97da65f685813acf63838de53074d87e5
alaeddine <boufaden.alaeddine.dev@gmail.com>
2022-10-07 15:26:45 +0100

    number of occurences

---
+++ leetcode/Number_of_Occurrences/solution.go
@@ -0,0 +1,15 @@
+func uniqueOccurrences(arr []int) bool {
+	freq, m := map[int]int{}, map[int]bool{}
+	for _, v := range arr {
+		freq[v]++
+	}
+	for _, v := range freq {
+		if _, ok := m[v]; !ok {
+			m[v] = true
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
