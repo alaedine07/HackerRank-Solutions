c76ff985c6eb275d45b329408983d1834f1c21e0
alaedine07 <alaedineboufaden@gmail.com>
2022-10-29 18:14:30 +0100

    backtracking problem

---
+++ leetcode/generate_parentheses/solve.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+        
+        def backtracking(open_count, closed_count):
+            if open_count == closed_count == n:
+                res.append("".join(stack))
+                return
+            if open_count < n:
+                stack.append("(")
+                backtracking(open_count + 1, closed_count)
+                stack.pop()
+            if closed_count < open_count:
+                stack.append(")")
+                backtracking(open_count, closed_count + 1)
+                stack.pop()
+        backtracking(0, 0)
